f2349561179b7727c18a9d62ee1d461c8f76b2d9
Enzo <89558747+enzon19@users.noreply.github.com>
2022-01-30 15:19:27 -0300

    Legacy codes

--- codes/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-const path = __dirname + '/status.html'
-
-app.get('/', (req, res) => res.sendFile(path));
-
-module.exports = () => {
-  app.listen(3000, ()=>{console.log("O servidor está supimpamente bem.")});
-}
