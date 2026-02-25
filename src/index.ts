fbca03b80909ac712f31a6c36572940487d3b8c5
Enzo <enzonbarata@outlook.com>
2026-02-25 19:10:17 -0300

    feat: error handler

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-import { bot } from "./bot/bot";
-import { server as createServer } from "./server";
+import { GrammyError, HttpError } from 'grammy';
+import { bot } from './bot/bot';
+import { server as createServer } from './server';
 const PORT = process.env.PORT;
 
 async function main() {
-  const server = await createServer();
-  server.listen({ port: Number(PORT) || 2608 }, (err, address) => {
-    if (err) console.error(err);
-    console.info(`Dicionário Bot funcionando em ${address}.`);
-  });
+	const server = await createServer();
+	server.listen({ port: Number(PORT) || 2608 }, (err, address) => {
+		if (err) console.error(err);
+		console.info(`Dicionário Bot funcionando em ${address}.`);
+	});
 
-  bot.start();
-  process.once("SIGINT", () => bot.stop());
-  process.once("SIGTERM", () => bot.stop());
+	bot.start();
+	process.once('SIGINT', () => bot.stop());
+	process.once('SIGTERM', () => bot.stop());
+
+	process.once('unhandledRejection', (reason, promise) => console.error(reason, promise));
+	process.once('uncaughtException', (reason, origin) => console.error(reason, origin));
+	bot.catch((err) => {
+		const ctx = err.ctx;
+		console.error(`Error while handling update ${ctx.update.update_id}:`);
+		const e = err.error;
+		if (e instanceof GrammyError) {
+			console.error('Error in request:', e.description);
+		} else if (e instanceof HttpError) {
+			console.error('Could not contact Telegram:', e);
+		} else {
+			console.error('Unknown error:', e);
+		}
+	});
 }
 
 main();
