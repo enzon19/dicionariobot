067479e5968693fb8a0ee77ea1ea0eaeb2098562
Enzo <enzonbarata@outlook.com>
2026-02-26 18:43:33 -0300

    feat: text listener

--- src/bot/listeners/index.ts
+++ src/bot/listeners/index.ts
@@ -1,8 +1,9 @@
 import type { Bot, Context } from 'grammy';
 import type { Listener } from '../../models/Listener';
 import { AddedToGroupListener } from './AddedToGroup';
+import { TextListener } from './Text';
 
-const listeners: Listener[] = [new AddedToGroupListener()];
+const listeners: Listener[] = [new AddedToGroupListener(), new TextListener()];
 
 export function registerListeners(bot: Bot<Context>): void {
 	for (const listener of listeners) {
