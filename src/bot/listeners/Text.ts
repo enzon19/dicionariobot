8acd2ecd68d3e6034bb390087185edc469cbb4b9
Enzo <enzonbarata@outlook.com>
2026-02-26 18:43:49 -0300

    feat: text listener with reply word request

---
+++ src/bot/listeners/Text.ts
@@ -0,0 +1,68 @@
+import type { Context, FilterQuery } from 'grammy';
+import { Listener } from '../../models/Listener';
+import type { Message } from 'grammy/types';
+import { buildWaitingReplyMessage } from '../../utils/messagesBuilders';
+import removeTelegramHTML from '../../utils/removeTelegramHTML';
+import getMeaningMessage from '../messages/meaningMessage';
+import getSynonymsMessage from '../messages/synonymsMessage';
+import getSentencesMessage from '../messages/sentencesMessage';
+const BOT_USERNAME = process.env.BOT_USERNAME;
+
+export class TextListener extends Listener {
+	on: FilterQuery[] = ['message:text', 'message:caption'];
+	saveUserData = false;
+
+	handle = (ctx: Context) => {
+		const message = ctx.message;
+		const text = message?.text || message?.caption;
+		if (!text) return;
+
+		if (message.reply_to_message?.text && message.reply_to_message.from?.username == BOT_USERNAME) {
+			// if it has reply_to_message from bot, so it could be the empty message from a command
+			handleReply(message, text, ctx);
+		} else if (message.chat.type == 'private') {
+			// if it's from a private chat, it's a shortcut
+			console.log('shortcut');
+		} else if (message.chat.type == 'group' || message.chat.type == 'supergroup') {
+			// if it's from a group, check for mistakes
+			console.log('check for mistakes');
+		}
+	};
+}
+
+async function handleReply(message: Message, text: string, ctx: Context) {
+	const repliedMessageText = message.reply_to_message?.text;
+
+	// could be asking for a word for meanings, synonyms, sentences
+	const waitingReplyMessage = removeTelegramHTML(buildWaitingReplyMessage('').slice(0, -2));
+	if (repliedMessageText?.startsWith(waitingReplyMessage)) {
+		handleWordRequest(message, text, ctx, repliedMessageText, waitingReplyMessage.length + 1);
+	}
+}
+
+type WordAction = 'definir' | 'sinônimos' | 'exemplos';
+async function handleWordRequest(
+	message: Message,
+	text: string,
+	ctx: Context,
+	repliedMessageText: string,
+	start: number
+) {
+	const action = repliedMessageText.slice(start, -1) as WordAction;
+
+	const handlers: Record<WordAction, (word: string) => Promise<string>> = {
+		definir: getMeaningMessage,
+		sinônimos: getSynonymsMessage,
+		exemplos: getSentencesMessage
+	};
+
+	ctx.replyWithChatAction('typing');
+	const result = await handlers[action](text);
+
+	ctx.reply(result, {
+		parse_mode: 'HTML',
+		reply_parameters: message && {
+			message_id: message?.message_id
+		}
+	});
+}
