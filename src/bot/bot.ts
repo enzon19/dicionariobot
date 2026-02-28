0c62f7a2eb865a049f7bea3211e846527ccc3b00
Enzo <enzonbarata@outlook.com>
2026-02-28 15:15:05 -0300

    chore: changed error message

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -3,7 +3,7 @@ import { registerCommands } from './commands';
 import { registerListeners } from './listeners';
 const TG_TOKEN = process.env.TG_TOKEN;
 
-if (!TG_TOKEN) throw new Error('TG_TOKEN não definido');
+if (!TG_TOKEN) throw new Error('Missing TG_TOKEN in environment variables.');
 
 export const bot = new Bot(TG_TOKEN);
 registerCommands(bot);
