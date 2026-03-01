b0300a837f8b07c887485719b04621b0df911555
Enzo <enzonbarata@outlook.com>
2026-03-01 20:48:36 -0300

    feat: initial session data

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,5 +1,5 @@
-import { Bot, Context, type SessionFlavor } from 'grammy';
-import { registerCommands } from './commands';
+import { Bot, Context, session, type SessionFlavor } from 'grammy';
+import { registerCommands, registerMenus } from './commands';
 import { registerListeners } from './listeners';
 import { autoQuote } from '@roziscoding/grammy-autoquote';
 import { registerStatelessQuestions } from './questions';
@@ -8,9 +8,18 @@ const TG_TOKEN = process.env.TG_TOKEN;
 
 if (!TG_TOKEN) throw new Error('Missing TG_TOKEN in environment variables.');
 
+const initialSessionData = {
+	settings: {
+		searchEngines: {
+			editing: {}
+		}
+	}
+};
+
 export type BotContext = Context & SessionFlavor<SessionData>;
 export const bot = new Bot<BotContext>(TG_TOKEN);
 bot.use(autoQuote());
+bot.use(session({ initial: () => initialSessionData }));
 
 registerStatelessQuestions(bot);
 registerCommands(bot);
