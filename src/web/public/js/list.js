2c3d291064d5886e9162471058eec932c684b025
Enzo <enzonbarata@outlook.com>
2026-03-02 19:41:42 -0300

    refactor: list page

--- src/web/public/js/list.js
+++ src/web/public/js/list.js
@@ -1,16 +1,28 @@
-async function addList () {
-  const dataURL = 'https://raw.githubusercontent.com/enzon19/dicionariobot/main/src/assets/json/mistakes.json';
-  const mistakes = await (await fetch(dataURL)).json();
-  let tableWithWords = '';
+async function addList() {
+	const dataURL = 'https://raw.githubusercontent.com/enzon19/dicionariobot/main/src/assets/json/mistakes.json';
+	const response = await fetch(dataURL);
 
-  for (const mistake of mistakes) {
-    tableWithWords += `<tr class="tableRow">
-  <td class="tableCell">${mistake.wrong}</td>
-  <td class="tableCell">${mistake.right}</td>
-</tr>`;
-  }
+	const mistakes = response.ok ? await response.json() : [];
+	const tbody = document.getElementById('mistakesList');
+	tbody.innerHTML = '';
 
-  document.getElementById('mistakesList').innerHTML = tableWithWords;
+	if (mistakes.length == 0) tbody.innerHTML = '<tr><td colspan="2" class="text-center pt-2">Sem palavras.</td></tr>';
+
+	for (const { wrong, right } of mistakes) {
+		const tr = document.createElement('tr');
+		tr.className = 'tableRow';
+
+		const tdWrong = document.createElement('td');
+		tdWrong.className = 'tableCell';
+		tdWrong.textContent = wrong;
+
+		const tdRight = document.createElement('td');
+		tdRight.className = 'tableCell';
+		tdRight.textContent = right;
+
+		tr.append(tdWrong, tdRight);
+		tbody.appendChild(tr);
+	}
 }
 
 document.addEventListener('DOMContentLoaded', addList);
