7e9f1270c9a80752ef92c7fd38b708ce39df9081
Enzo <enzonbarata@outlook.com>
2026-03-02 14:29:17 -0300

    feat: check for mistakes

--- src/bot/listeners/Text.ts
+++ src/bot/listeners/Text.ts
@@ -1,10 +1,11 @@
 import type { Context, FilterQuery } from 'grammy';
+import { type Shortcut } from '../../db/schema';
 import { Listener } from '../../models/Listener';
 import getMeaningMessage from '../messages/meaningMessage';
 import getSynonymsMessage from '../messages/synonymsMessage';
 import getSentencesMessage from '../messages/sentencesMessage';
 import { getUserShortcuts, saveUserLastUse } from '../../services/users';
-import { type Shortcut } from '../../db/schema';
+import fs from 'fs';
 
 export class TextListener extends Listener {
 	listenerName = 'text-message';
@@ -25,9 +26,12 @@ export class TextListener extends Listener {
 				await saveUserLastUse(ctx.from.id, {
 					type: this.listenerName + (isSlashShortcut ? ':slash-shortcut' : ':shortcut')
 				});
-		} else if (message.chat.type == 'group' || message.chat.type == 'supergroup') {
-			// if it's from a group, check for mistakes
-			console.log('check for mistakes');
+		} else if (!message.from.is_bot && (message.chat.type == 'group' || message.chat.type == 'supergroup')) {
+			// if it's from a group and user is not a bot, check for mistakes
+			if ((await checkForMistakes(ctx, text)) && ctx.from)
+				await saveUserLastUse(ctx.from.id, {
+					type: this.listenerName + ':mistakes'
+				});
 		}
 	};
 }
@@ -51,4 +55,32 @@ async function shortcut(ctx: Context, text: string, slash: boolean) {
 	ctx.reply(result, {
 		parse_mode: 'HTML'
 	});
+}
+
+// CHECK FOR MISTAKES
+type Mistake = {
+	wrong: string;
+	right: string;
+};
+const mistakesList: Mistake[] = JSON.parse(fs.readFileSync('public/mistakes.json', 'utf-8'));
+const mistakeMap = new Map(mistakesList.map((m) => [m.wrong, m.right]));
+
+async function checkForMistakes(ctx: Context, text: string) {
+	const normalizedText = text
+		.toLowerCase()
+		.replace(/[\p{P}]/gu, '')
+		.trim();
+	const words = normalizedText.split(/\s+/).filter((e) => e);
+
+	const corrections = new Set<string>();
+
+	for (const word of words) {
+		const correction = mistakeMap.get(word);
+		if (correction) corrections.add(correction + '*');
+	}
+
+	if (corrections.size == 0) return false;
+
+	await ctx.reply([...corrections].join('\n'));
+	return true;
 }
