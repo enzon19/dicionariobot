11a09ee66e57d7105fe171a12328dabd00e3c762
Enzo <enzonbarata@outlook.com>
2026-03-02 15:35:47 -0300

    feat: initial web setup

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,7 +1,14 @@
 import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
 
 export async function server() {
 	const fastify = Fastify();
+
+	fastify.register(fastifyStatic, {
+		root: path.join(__dirname, 'public'),
+		extensions: ['html']
+	});
 
 	return fastify;
 }
