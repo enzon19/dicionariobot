7226945947f113bc9842d43c3a3e4d687cba619d
Enzo <enzonbarata@outlook.com>
2026-03-03 15:31:06 -0300

    refactor: shortcuts also using replyWithWordResult

--- src/bot/listeners/Text.ts
+++ src/bot/listeners/Text.ts
@@ -1,33 +1,31 @@
-import type { Context, FilterQuery } from 'grammy';
+import type { FilterQuery } from 'grammy';
+import type { BotContext } from '../bot';
 import { Listener } from '../../models/Listener';
 import { saveUserLastUse } from '../../services/users';
-import { buildShortcutMessage } from '../messages/shortcutsMessage';
+import { chooseShortcutMessage } from '../messages/shortcutsMessage';
 import { checkForMistakesAndBuildMessage } from '../messages/mistakesMessage';
+import { replyWithWordResult } from '../../utils/handleWordQueryCommand';
 
 export class TextListener extends Listener {
 	listenerName = 'text-message';
 	on: FilterQuery[] = ['message:text', 'message:caption'];
 	saveUserData = false;
 
-	handle = async (ctx: Context) => {
+	handle = async (ctx: BotContext) => {
 		const message = ctx.message;
 		const text = message?.text || message?.caption;
 		if (!text) return;
 
-		if (message.chat.type == 'private') {
+		if (message.chat.type == 'private' && ctx.from) {
 			// if it's from a private chat, it's a shortcut
 			const isSlashShortcut = text.startsWith('/');
 
-			const shortcutMessage = await buildShortcutMessage(ctx, text, isSlashShortcut);
-			if (shortcutMessage)
-				await ctx.reply(shortcutMessage, {
-					parse_mode: 'HTML'
-				});
+			const shortcutMessageBuilder = await chooseShortcutMessage(ctx.from.id, isSlashShortcut);
+			await replyWithWordResult(ctx, shortcutMessageBuilder, text);
 
-			if (ctx.from)
-				await saveUserLastUse(ctx.from.id, {
-					type: this.listenerName + (isSlashShortcut ? ':slash-shortcut' : ':shortcut')
-				});
+			await saveUserLastUse(ctx.from.id, {
+				type: this.listenerName + (isSlashShortcut ? ':slash-shortcut' : ':shortcut')
+			});
 		} else if (!message.from.is_bot && (message.chat.type == 'group' || message.chat.type == 'supergroup')) {
 			// if it's from a group and user is not a bot, check for mistakes
 			const mistakes = await checkForMistakesAndBuildMessage(ctx, text);
--- src/bot/messages/shortcutsMessage.ts
+++ src/bot/messages/shortcutsMessage.ts
@@ -1,23 +1,16 @@
-import type { Context } from 'grammy';
 import { type Shortcut } from '../../db/schema';
 import getMeaningMessage from '../messages/meaningMessage';
 import getSynonymsMessage from '../messages/synonymsMessage';
 import getSentencesMessage from '../messages/sentencesMessage';
 import { getUserShortcuts } from '../../services/users';
 
-export async function buildShortcutMessage(ctx: Context, text: string, slash: boolean) {
-	if (!ctx.from) return;
-
-	await ctx.replyWithChatAction('typing');
-
+export async function chooseShortcutMessage(userID: number, slash: boolean) {
 	const handlers: Record<Shortcut, (word: string, userID: number) => Promise<string>> = {
 		meanings: getMeaningMessage,
 		synonyms: getSynonymsMessage,
 		sentences: getSentencesMessage
 	};
 
-	const userChosenShortcut = await getUserShortcuts(ctx.from.id, slash ? 'slash' : 'regular');
-
-	const result = await handlers[userChosenShortcut](text, ctx.from.id);
-	return result;
+	const userChosenShortcut = await getUserShortcuts(userID, slash ? 'slash' : 'regular');
+	return handlers[userChosenShortcut];
 }
